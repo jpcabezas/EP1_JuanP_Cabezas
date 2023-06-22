69f01b7ac2474ba6c440b81aae70747c6cb8b7af
JUAN PABLO CABEZAS TORRES <e1724476997@uisrael.edu.ec>
2023-06-22 02:26:05 -0500

    Agregar archivos de proyecto.

---
+++ EP1_JuanP_Cabezas/Resumen.xaml.cs
@@ -0,0 +1,40 @@
+﻿using EP1_JuanP_Cabezas.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Xamarin.Forms.Internals.Profile;
+
+namespace EP1_JuanP_Cabezas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(String dato1, string dato2, string dato3, string dato5, string dato6, string dato7, string dato8)
+        {
+            InitializeComponent();
+            txtnombre.Text = dato1;
+            txtApellido.Text = dato2;
+            txtEdad.Text = dato3;
+            txtPais.Text = dato5;
+            txtCiudad.Text = dato6;
+            txtMonto.Text = dato7;
+            txtPago.Text = dato8;
+            
+
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
+
+            Navigation.PushAsync(new Login());
+           
+
+        }
+    }
+}
---
+++ EP1_JuanP_Cabezas/registro.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EP1_JuanP_Cabezas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        string usuario;
+        public registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "usuario conectado" + " " + "Es" + " " + usuario;
+            this.usuario = usuario;
+        }
+
+        private void btnResumen_Clicked(object sender, EventArgs e)
+        {
+            string dato1 = txtNombre.Text;
+            string dato2 = txtApellido.Text;
+            string dato3 = txtEdad.Text;
+            string dato5 = dpPais.SelectedItem.ToString();
+            String dato6 = dpCiudad.SelectedItem.ToString();
+            string dato7 = txtMonto.Text;
+            string dato8 = txtPago.Text;
+            
+            Navigation.PushAsync(new Resumen(dato1,dato2,dato3,dato5,dato6,dato7,dato8));
+        }
+
+        private void btnPago_Clicked(object sender, EventArgs e)
+        {
+
+            double Monto = Convert.ToDouble(txtMonto.Text);
+            double Pago1 = (1800 *0.05);
+            double Pago2 = (Pago1 + Monto);
+            double calcularPago = (1800 - Pago2)/3;
+            txtPago.Text = calcularPago.ToString();
+
+        }
+        private void dpPais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+
+        private void dpCiudad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+ }
